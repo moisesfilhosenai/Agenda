c0d393a40641b77d414eeb6cf2b74ba57696bf69
Moises Olimpio <moises.filho@docente.senai.br>
2023-12-18 18:48:45 -0300

    Subindo projeto

---
+++ app/src/main/java/br/com/senai/agenda/activity/MainActivity.java
@@ -0,0 +1,16 @@
+package br.com.senai.agenda.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import br.com.senai.agenda.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
