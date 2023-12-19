8c727403943a473a7d524bf1edb6ce2fdc0bb816
Moises Olimpio <moises.filho@docente.senai.br>
2023-12-19 19:29:23 -0300

    Criando adapter

---
+++ app/src/main/java/br/com/senai/agenda/adapter/ContatoAdapter.java
@@ -0,0 +1,65 @@
+package br.com.senai.agenda.adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+import br.com.senai.agenda.R;
+import br.com.senai.agenda.model.Contato;
+
+public class ContatoAdapter
+        extends RecyclerView.Adapter<ContatoAdapter.ContatoViewHolder>  {
+
+    private List<Contato> contatos;
+    private Context context;
+
+    public ContatoAdapter(List<Contato> contatos,
+                          Context context) {
+        this.contatos = contatos;
+        this.context = context;
+    }
+
+    // Cria um item do view holder
+    // Especificamos o layout que sera usado
+    @NonNull
+    @Override
+    public ContatoViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(this.context)
+                .inflate(R.layout.item_contato, parent, false);
+        return new ContatoViewHolder(view);
+    }
+
+    // Faz a troca do conteúdo
+    // Dentro do arquivo de layout
+    @Override
+    public void onBindViewHolder(@NonNull ContatoViewHolder holder, int position) {
+        Contato contato = contatos.get(position);
+        holder.tvNome.setText(contato.getNome());
+    }
+
+    // Conta a qtde de itens da lista
+    @Override
+    public int getItemCount() {
+        return this.contatos.size();
+    }
+
+    public class ContatoViewHolder
+            extends RecyclerView.ViewHolder {
+
+        TextView tvNome;
+
+        // Método para criar novos View Holder
+        // Chamado pelo Layout Manager
+        public ContatoViewHolder(@NonNull View itemView) {
+            super(itemView);
+            tvNome = itemView.findViewById(R.id.tvNome);
+        }
+    }
+}
