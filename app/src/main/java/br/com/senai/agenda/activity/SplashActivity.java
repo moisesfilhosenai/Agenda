e401ecf453015517f1992894da9ff72457a52fce
Moises Olimpio <moises.filho@docente.senai.br>
2023-12-18 21:41:20 -0300

    Atualizando projeto

--- app/src/main/java/br/com/senai/agenda/activity/SplashActivity.java
+++ app/src/main/java/br/com/senai/agenda/activity/SplashActivity.java
@@ -2,7 +2,11 @@ package br.com.senai.agenda.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 import br.com.senai.agenda.R;
 
@@ -12,5 +16,19 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        new Timer().schedule(new TimerTask() {
+            @Override
+            public void run() {
+                carregaMain();
+            }
+        }, 3000);
+    }
+
+    private void carregaMain() {
+        Intent telaPrincipal = new Intent(
+                this, MainActivity.class);
+        startActivity(telaPrincipal);
+        finish();
     }
 }
